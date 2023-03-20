83b27b07bff2de551bb34b55a4e108aa58b80c49
NguyenTriBao <86540179+NguyenTriBao@users.noreply.github.com>
2023-03-20 21:40:17 +0700

    Add files via upload

---
+++ Profolio/index.php
@@ -0,0 +1,68 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
+    <link rel="stylesheet" href="./public/css/style.css">
+</head>
+<body>
+    <div class="container">
+        <div class="row">
+            <div class="col-md-4 col4">
+                <img src="./public/Image/avata1.jpg" alt="#">
+                <h3>Contact</h3>
+                Email: <a href="mailto:tribao0510@gmail.com">tribao0510@gmail.com</a>
+                <br>Phone: 0368390949
+                <br>Date of birth: 05-10-2002
+                <br>adress: 17/6, 15 street, Linh Chieu awards, Thu Duc city, HoChiMinh city
+                <h3>Hard skills</h3>
+                Programming: C#, CSS, HTML, Java, Javascript, PHP, Laravel
+                <br>Data processing: SQL
+                <br>Databace: MySQL, SQL sever
+                <h3>Foreign language</h3>
+                English
+            </div>
+            <div class="col-md-8 col8">
+                <h1>Nguyen Tri Bao</h1>
+                <h2>Web Programmer</h2>
+                <a href="data:application/octet-stream;base64,<?php echo base64_encode(file_get_contents('Resume.pdf')); ?>">
+                <button id="resume"  download="Resume_BaoNguyen.pdf"> Download Resume</button>
+                </a>
+                <a href="https://github.com/NguyenTriBao">
+                <button id="github"> My github</button>
+                </a>
+                <h3>Carreer objective</h3>
+                Utilize PHP and other programming skills to develop a platform that would help improve and engage company's customer retention
+                <h3>Experience</h3>
+                <h4>Develop shopping with PHP</h4>
+                Thu Duc college of technology/ November 2021- January 2022
+                <br> Write a program to design a order web site interface for a shopping by PHP. Customer can see and order some product on web site
+                <br> My github:
+                <a href="https://github.com/NguyenTriBao/nhom8_ST3_BE1_NH21">https://github.com/NguyenTriBao/nhom8_ST3_BE1_NH21</a>
+                <h4>Develop shopping with Laravel</h4>
+                Thu Duc college of technology/ March 2022- May 2022
+                <br>Write a program to design  a order web site interface for a shopping. Customer can see and order some product on web site
+                <br> My github:
+                <a href="https://hithub.com/vanhaust1134/NH21_22_BE2_CT2_nhom13">https://hithub.com/vanhaust1134/NH21_22_BE2_CT2_nhom13</a>
+                <h3>Education</h3>
+                October 2020 - Present
+                <br> Faculty: Thu Duc college of technology
+                <br>Major: information of technology
+                <div class="footer">
+                <h3>Reference</h3>
+                Consultant: Bui Thi Phuong Thao
+                <br>Email: <a href="mailto:phuongthao2145@gmail.com">phuongthao2145@gmail.com</a>
+                </div>
+               
+            </div>
+    
+        </div>
+    </div>
+  
+   
+    <script src="./public/js/main.js"></script>
+</body>
+</html>
---
+++ Profolio/public/js/main.js
@@ -0,0 +1,12 @@
+
+
+
+// const downloadBtn = document.getElementById('download-btn');
+
+// downloadBtn.addEventListener('click', () => {
+//   const pdfUrl = './Resume.pdf';
+//   const downloadLink = document.createElement('a');
+//   downloadLink.setAttribute('href', pdfUrl);
+//   downloadLink.setAttribute('download', 'resume.pdf');
+//   downloadLink.click();
+// });
